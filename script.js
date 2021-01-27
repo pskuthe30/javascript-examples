9fd3d26e6e966275cd63930f1b6d522270670de2
pskuthe@gmail.com <pskuthe@gmail.com>
2021-01-27 10:23:42 -0500

    Form a # triangle and square

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 "use strict";
 function printSquare(width) {
   for (let i = 1; i <= width; i++) {
+    let pound = "";
     for (let j = 1; j <= width; j++) {
-      console.log(`#`);
+      pound += "#";
     }
-    console.log(`\n`);
+    console.log(pound);
   }
 }
 printSquare(3);
+
+const printTriangle = (width) => {
+  for (let i = 0; i <= width; i++) {
+    let row = "";
+    for (let j = 0; j < i; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+};
+printTriangle(4);
